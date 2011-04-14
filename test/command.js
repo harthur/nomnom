2d3139442b6129004d5efb7cd8c53e393f375f36
Heather Arthur <fayearthur@gmail.com>
2011-04-13 21:35:34 -0700

    chain options for command-based

--- test/command.js
+++ test/command.js
@@ -14,33 +14,37 @@ var opts = {
 }
 var parser = nomnom(opts);
 
-var browserOpts = {
-  port: {
-    string: "-p PORT, --port=PORT",
-    default: 3000,
-    help: "port to run test server on"
-  }
-}
-parser.command('browser', browserOpts, function(options) {
-  assert.ok(false, "shouldn't call callback for 'browser' command")
-});
 
-var nodeOpts = {
-  filename: {
-    position: 1,
-    help: "test file to run in node"
-  },
-  config: {
-    string: "-c FILE, --config=FILE",
-    default: 'config.json',
-    help: "json file with tests to run"
-  }
-};
+parser.command('browser')
+  .opts({
+    port: {
+      string: "-p PORT, --port=PORT",
+      default: 3000,
+      help: "port to run test server on"
+    }
+  })
+  .callback(function(options) {
+    assert.ok(false, "shouldn't call callback for 'browser' command");
+  });
 
-parser.command('node', nodeOpts, function(options) {
-  assert.equal(options.filename, "test.js");
-  assert.equal(options.config, "test.json");
-  assert.ok(options.debug, "should pick up global arg");
-}, "** Run all the node tests **");
+parser.command('node')
+  .opts({
+    filename: {
+      position: 1,
+      help: "test file to run in node"
+    },
+    config: {
+      string: "-c FILE, --config=FILE",
+      default: 'config.json',
+      help: "json file with tests to run"
+    }
+  })
+  .callback(function(options) {
+    sys.puts('in node command');
+    assert.equal(options.filename, "test.js");
+    assert.equal(options.config, "test.json");
+    assert.ok(options.debug, "should pick up global arg");
+  })
+  .help("** Run all the node tests **");
 
 parser.parseArgs(["node", "test.js", "-c", "test.json"]);
