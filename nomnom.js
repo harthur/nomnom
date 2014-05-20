4239fcbc40325d70b951e61a033ab5d1a56a7f0a
James Socol <me@jamessocol.com>
2014-05-19 21:57:48 -0400

    Add value processing callback. Fix #28.
    Adds a "process" key to options that allows a callback to arbitrarily
    modify the value.

--- nomnom.js
+++ nomnom.js
@@ -460,6 +460,10 @@ ArgParser.prototype.setOption = function(options, arg, value) {
      catch(e) {}
   }
 
+  if (option.transform) {
+     value = option.transform(value);
+  }
+
   var name = option.name || arg;
   if (option.choices && option.choices.indexOf(value) == -1) {
      this.print(name + " must be one of: " + option.choices.join(", "), 1);
